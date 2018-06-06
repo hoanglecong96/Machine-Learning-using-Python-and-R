860a8d314044400657d1cf86f6d1822b7db5366b
Hoang Le <39092351+hoanglecong96@users.noreply.github.com>
2018-06-05 23:51:02 -0400

    Full Data Preprocessing Template R

---
+++ Data Preprocessing Template.R
@@ -0,0 +1,31 @@
+# Data Preprocessing Template
+setwd("C:/Users/Lee TyT/Google Drive/Coding Material/Machine Learning Course Udemy (Kirill and Hadelin)/Machine Learning A-Z/Part 1 - Data Preprocessing")
+# Importing the dataset
+dataset = read.csv('Data.csv')
+
+#Taking care of Missing Data
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN=function(x) mean(x,na.rm=TRUE)),
+                     dataset$Age)
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN=function(x) mean(x,na.rm=TRUE)),
+                        dataset$Salary)
+#Encoding Categorical Data
+dataset$Country=factor(dataset$Country,
+                       levels=c('France','Spain','Germany'),
+                       labels=c(1,2,3))
+dataset$Purchased=factor(dataset$Purchased,
+                        levels=c('No','Yes'),
+                        labels=c(0,1))
+#Splitting the dataset into training set and test set
+install.packages('caTools')
+library(caTools)
+set.seed(123)
+split=sample.split(dataset$Purchased,SplitRatio=0.8)
+training_set=subset(dataset, split==TRUE)
+test_set=subset(dataset, split==FALSE)
+
+#Feature Scaling
+training_set[,2:3]=scale(training_set[,2:3])
+test_set[,2:3]=scale(test_set[,2:3])
+
